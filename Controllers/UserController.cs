5009062e50371ee5e4983b78c9db291a0980ec4a
R-A-Medeiros <r.medeiros1206@gmail.com>
2024-11-19 23:17:52 -0300

    Criação do projeto / endpoint teste

---
+++ Controllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyFirstAPI.Models;
+using System.Net.Cache;
+
+namespace MyFirstAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UserController : ControllerBase
+{
+    [HttpGet]
+    [ProducesResponseType(typeof(Response), StatusCodes.Status200OK)]
+    public IActionResult Get()
+    {
+        var response = new Response
+        {
+            Age = 23,
+            Name = "Rinaldo"
+        };
+
+        return Ok(response);
+    }
+
+}
---
+++ Models/Response.cs
@@ -0,0 +1,7 @@
+﻿namespace MyFirstAPI.Models;
+
+public class Response
+{
+    public string Name {  get; set; }
+    public int Age { get; set; }
+}
